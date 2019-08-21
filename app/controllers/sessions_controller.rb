864f3f557b159f501b7365cac29b5d787f3e53e1
Savannah C <sgcorrell@gmail.com>
2019-08-21 19:10:16 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    session[:name] = params[:name]
-    redirect_to "/"
+    if params[:name]
+      session[:name] = params[:name]
+      redirect_to "/"
+    else
+      redirect_to "/login"
+    end
   end
   
   def destroy
